2c79039fe3cae8b8e4290648bf736052b46c1add
macOS <warmlab@me.com>
2016-09-18 19:23:32 +0800

    cut something not needed at present

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,17 @@
 from flask import Flask
-from flask.ext.bootstrap import Bootstrap
-from flask.ext.mail import Mail
-from flask.ext.moment import Moment
-from flask.ext.sqlalchemy import SQLAlchemy
-from flask.ext.login import LoginManager
-from flask.ext.pagedown import PageDown
+#from flask.ext.bootstrap import Bootstrap
+#from flask_mail import Mail
+#from flask.ext.moment import Moment
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+#from flask.ext.pagedown import PageDown
 from config import config
 
-bootstrap = Bootstrap()
-mail = Mail()
-moment = Moment()
+#bootstrap = Bootstrap()
+#mail = Mail()
+#moment = Moment()
 db = SQLAlchemy()
-pagedown = PageDown()
+#pagedown = PageDown()
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
@@ -23,25 +23,25 @@ def create_app(config_name):
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
 
-    bootstrap.init_app(app)
-    mail.init_app(app)
-    moment.init_app(app)
+    #bootstrap.init_app(app)
+    #mail.init_app(app)
+    #moment.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
-    pagedown.init_app(app)
+    #pagedown.init_app(app)
 
     if not app.debug and not app.testing and not app.config['SSL_DISABLE']:
-        from flask.ext.sslify import SSLify
+        from flask_sslify import SSLify
         sslify = SSLify(app)
 
     from .product import product as product_blueprint
     app.register_blueprint(product_blueprint)
 
-    from .auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint, url_prefix='/auth')
+    #from .auth import auth as auth_blueprint
+    #app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
-    from .sale import sale as sale_blueprint
-    app.register_blueprint(sale_blueprint)
+    #from .sale import sale as sale_blueprint
+    #app.register_blueprint(sale_blueprint)
 
     from .shop import shop as shop_blueprint
     app.register_blueprint(shop_blueprint, url_prefix='/shop')
--- app/models.py
+++ app/models.py
@@ -2,8 +2,8 @@ from datetime import datetime
 import hashlib
 from werkzeug.security import generate_password_hash, check_password_hash
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
-from markdown import markdown
-import bleach
+#from markdown import markdown
+#import bleach
 from flask import current_app, request, url_for
 from flask.ext.login import UserMixin, AnonymousUserMixin
 from app.exceptions import ValidationError
