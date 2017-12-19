483e971999183b5b6692a01ad7e42f6e07dada0c
George <georgelee@techtrex.com>
2017-12-19 11:00:17 +0800

    add api module

---
+++ app/api/product.py
@@ -0,0 +1,80 @@
+from time import time
+from datetime import datetime
+from decimal import Decimal
+
+from flask import render_template, redirect
+from flask import request, url_for, abort, json
+from flask.json import jsonify
+
+from flask_login import login_required, current_user, login_user
+
+from flask_sqlalchemy import Pagination
+
+from . import api
+
+from .. import db
+
+from ..decorators import member_required
+
+from ..models import ProductCategory, Product
+from ..models import Shoppoint
+#from ..models import Shoppoint, Product, Address
+#from ..models import Member, UserAuth
+#from ..models import Ticket, TicketProduct, TicketAddress
+
+
+@api.route('/categories/', methods=['GET'])
+def categories():
+    sp = Shoppoint.query.first()
+    if not sp:
+        abort(404)
+
+    categories = ProductCategory.query.all()
+    return jsonify([c.to_json() for c in categories])
+
+@api.route('/products', methods=['GET'])
+def products():
+    sp = Shoppoint.query.first()
+    if not sp:
+        abort(404)
+
+    #page = request.args.get('page', type=int, default=1)
+    products = Product.query.filter_by(is_available_on_web=True)
+    #pagination = products.paginate(page=page, per_page=7, error_out=False)
+    #return render_template('api/list.html', products=products, shoppoint=sp, pagination=pagination)
+    return '{"a":"b"}'
+
+@api.route('/product', methods=['GET'])
+def product():
+    sp = Shoppoint.query.first()
+    if not sp:
+        abort(404)
+    #code = 1
+    #product = Product.query.filter_by(code=code).first()
+    #ppc = {}
+    #for pp in product.parameters:
+    #    if pp.parameter.category not in ppc:
+    #        ppc[pp.parameter.category] = [pp]
+    #    else:
+    #        ppc[pp.parameter.category].append(pp)
+    #logger.debug(ppc)
+    #message = request.args.get('added');
+    #return render_template('api/detail.html', product=product, parameter_categories=ppc, message=message, shoppoint=sp)
+    return '{"a":"b"}'
+
+@api.route('/product/spec/<code>', methods=['GET'])
+def product_spec(code):
+    sp = Shoppoint.query.first()
+    if not sp:
+        about(404)
+
+    ppc = {}
+    product = Product.query.filter_by(code=code).first()
+    for pp in product.parameters:
+        if pp.parameter.category not in ppc:
+            ppc[pp.parameter.category.name] = [pp.to_json()]
+        else:
+            ppc[pp.parameter.category.name].append(pp.to_json())
+
+    #return jsonify([p.to_json() for p in c for c in ppc])
+    return jsonify(ppc)
